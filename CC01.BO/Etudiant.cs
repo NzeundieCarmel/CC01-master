1a04317e0719f7f887508fca0491140691db6135
cnzeundie908@gmail.com <cnzeundie908@gmail.com>
2020-11-20 13:34:27 +0100

    new

---
+++ CC01.BO/Etudiant.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC01.BO
+{
+    [Serializable]
+    public class Etudiant
+    {
+      
+        public byte[] Photo { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public DateTime Né_le { get; set; }
+        public string Matricule { get; set; }
+        public string Contact { get; set; }
+        public string Email { get; set; }
+
+
+        public Etudiant()
+        {
+
+        }
+
+        public Etudiant(byte[] photo, string nom, string prenom, DateTime né_le, 
+            string matricule, string contact, string email)
+        {
+            Photo = photo;
+            Nom = nom;
+            Prenom = prenom;
+            Né_le = né_le;
+            Matricule = matricule;
+            Contact = contact;
+            Email = email;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Etudiant etudiant &&
+                   Matricule == etudiant.Matricule;
+        }
+
+        public override int GetHashCode()
+        {
+            return 797189699 + EqualityComparer<string>.Default.GetHashCode(Matricule);
+        }
+    }
+}
---
+++ CC01.DAL/EtudiantDAO.cs
@@ -0,0 +1,71 @@
+﻿using CC01.BO;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CC01.DAL
+{
+    public class EtudiantDAO
+    {
+        private static List<Etudiant> etudiants;
+        private const string FILE_NAME = @"data/etudiants.json";
+        FileInfo file;
+
+        public EtudiantDAO()
+        {
+          
+        file = new FileInfo(FILE_NAME);
+
+            if(!file.Directory.Exists)
+            {
+                file.Directory.Create();
+            }
+     
+     
+            if(file.Exists)
+            {
+                file.Create().Close();
+            }
+            if(file.Length > 0)
+            {
+                using(StreamReader sr= new StreamReader(file.FullName))
+                {
+                    string Json = sr.ReadToEnd();
+                    etudiants = JsonConvert.DeserializeObject<List<Etudiant>>(Json);
+                }
+               
+            }
+            else
+            {
+                if(etudiants == null)
+                {
+                    etudiants = new List<Etudiant>();
+                }
+            }
+        }
+        public void Add(Etudiant etudiant)
+        {
+            etudiants.Add(etudiant);
+            using (StreamWriter sw = new StreamWriter(file.FullName, false))
+            {
+                string json = JsonConvert.SerializeObject(etudiants);
+                sw.WriteLine(json);
+            }
+        }
+        public void Delete(Etudiant etudiant)
+        {
+            etudiants.Remove(etudiant);
+            using (StreamWriter sw = new StreamWriter(file.FullName, false))
+            {
+                string json = JsonConvert.SerializeObject(etudiants);
+                sw.WriteLine(json);
+            }
+        }
+      
+    }
+}
